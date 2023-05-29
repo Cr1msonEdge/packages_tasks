a5df59fdf524c3052cbada7a9e84c250564d99af
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2023-05-30 01:30:10 +0300

    Add files via upload

---
+++ rabbitGraph/task1.m
@@ -0,0 +1,23 @@
+#x(n+1) = delta * xn * (1 - xn); уравнение плодящихся кроликов???? логистическое отображение
+
+#возьмём n = 10
+#delta = 1:0.1:4;
+delta = 3.9;
+xn = 0.500001;
+xn1 = 0.5;
+X = [];
+Y = [];
+X1 = [];
+Y1 = [];
+for i=1:100
+  xn = delta .* xn .* (1 - xn);
+  xn1 = delta .* xn1 .* (1 - xn1);
+  X(i) = i;
+  Y(i) = xn;
+  X1(i) = i;
+  Y1(i) = xn1;
+endfor
+plot(X, Y);
+hold on;
+plot(X1, Y1, 'red');
+hold off;
