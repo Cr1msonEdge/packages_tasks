2556656f25fde097be8765750494e88b0e21aa18
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2023-05-31 00:04:45 +0300

    Add files via upload

---
+++ fence/fence.m
@@ -0,0 +1,16 @@
+function A = fence (varargin)
+    input1 = 1;
+    input2 = 1;
+    if length(varargin) == 2
+      input1 = varargin{1};
+      input2 = varargin{2};
+    else
+      input1 = input2 = varargin{1};
+    endif
+    A = false(input1, input2);
+    for i = 1:2:input2
+      for j = 1:input1
+        A(j, i) = true;
+      endfor
+    endfor
+endfunction
