69f5699ac43441b358119c67b4c90cd209a0faf2
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2023-05-31 00:04:08 +0300

    Add files via upload

---
+++ checkboard/checkerboard.m
@@ -0,0 +1,19 @@
+function A = checkerboard(varargin)
+    input1 = 1;
+    input2 = 1;
+    if length(varargin) == 2
+      input1 = varargin{1};
+      input2 = varargin{2};
+    else
+      input1 = input2 = varargin{1};
+    endif
+    A = true(input1, input2);
+    for i = 1:input1
+      for j = 1:input2
+        if mod(i,2) == 0 && mod (j, 2) != 0 || mod(i,2) != 0 && mod(j,2) == 0
+            A(i,j) = false;
+        endif
+      endfor
+    endfor
+endfunction
+
